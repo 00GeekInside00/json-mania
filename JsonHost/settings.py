6207374a1c59b6a57f70be847e9561457ab1680d
Ahmed Helmi <ahmed.shawkat@protonmail.com>
2020-08-12 10:18:02 +0200

    setting generic relative paths

--- JsonHost/settings.py
+++ JsonHost/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'JsonHost.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,6 +144,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 CSRF_COOKIE_NAME = "csrftoken"
 LOGIN_REDIRECT_URL = 'StrorageList'
